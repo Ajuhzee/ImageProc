8ea70767d1cc88394a0355b36bf9f643ee9b838f
Ajuhzee <Ajuhzee@gmail.com>
2014-10-21 23:19:16 +0200

    Bild anzeigen ausgelagert

--- src/name/ajuhzee/imageproc/Main.java
+++ src/name/ajuhzee/imageproc/Main.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -69,7 +70,11 @@ public class Main extends Application {
 
 	@FXML
 	private void loadImageAction(ActionEvent e) {
-		ImageLoader.loadFile();
+		ImageLoader.loadFromFile();
+	}
+
+	public void setImage(Image img) {
+		imageView.setImage(img);
 	}
 
 	@FXML
