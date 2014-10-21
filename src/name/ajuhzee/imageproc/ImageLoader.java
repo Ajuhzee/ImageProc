e67dec5d35918799ac71e62e72d0507ceef5f049
Ajuhzee <Ajuhzee@gmail.com>
2014-10-21 17:27:20 +0200

    Bild in Variable gespeichert

--- src/name/ajuhzee/imageproc/ImageLoader.java
+++ src/name/ajuhzee/imageproc/ImageLoader.java
@@ -6,24 +6,24 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
 
 public class ImageLoader {
 
+	private static final Logger logger = Logger.getLogger(ImageLoader.class.getName());
+
 	public static void loadFile() {
 		FileChooser fc = new FileChooser();
 
 		File file = fc.showOpenDialog(null);
-
 		try {
 			BufferedImage img = ImageIO.read(file);
-			Image image = SwingFXUtils.toFXImage(img, null);
+			ImageProc.setImage(img);
 		} catch (IOException ex) {
-			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+			logger.log(Level.SEVERE, "File loading failed", ex);
 		}
+
 	}
 }
---
+++ src/name/ajuhzee/imageproc/ImageProc.java
@@ -0,0 +1,12 @@
+package name.ajuhzee.imageproc;
+
+import java.awt.image.BufferedImage;
+
+public class ImageProc {
+
+	private static BufferedImage image;
+
+	public static void setImage(BufferedImage img) {
+		image = img;
+	}
+}
