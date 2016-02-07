3340109bc3a52889a2e481654acdc4786adda8f9
Ajuhzee <ajuhzee@gmail.com>
2016-02-07 16:18:24 +0100

    Move ErodeDilate to filter package

--- src/main/java/name/ajuhzee/imageproc/plugin/image/process/filter/ErodeDilate.java
+++ src/main/java/name/ajuhzee/imageproc/plugin/image/process/filter/ErodeDilate.java
@@ -1,11 +1,11 @@
-package name.ajuhzee.imageproc.plugin.image.process;
+package name.ajuhzee.imageproc.plugin.image.process.filter;
 
 import javafx.scene.image.Image;
 import name.ajuhzee.imageproc.plugin.ImagePlugin;
 import name.ajuhzee.imageproc.plugin.MenuPositionBuilder;
+import name.ajuhzee.imageproc.plugin.PluginInformation;
 import name.ajuhzee.imageproc.plugin.PluginLoadException;
 import name.ajuhzee.imageproc.plugin.control.ImagePluginContext;
-import name.ajuhzee.imageproc.plugin.PluginInformation;
 import name.ajuhzee.imageproc.processing.ImageEditing;
 import name.ajuhzee.imageproc.view.ErodeDilateMenuController;
 
@@ -27,8 +27,8 @@ public class ErodeDilate extends ImagePlugin {
 	 * @throws PluginLoadException if context == null
 	 */
 	public ErodeDilate(ImagePluginContext context) throws PluginLoadException {
-		super(MenuPositionBuilder.topMenu("process", "Bearbeiten", 100).subMenu("erodeDilate",
-				PLUGIN_INFO).get(), PLUGIN_INFO, context);
+		super(MenuPositionBuilder.topMenu("process", "Bearbeiten", 100).subMenu("filter", "Filter")
+				.subMenu("erodeDilate", PLUGIN_INFO).get(), PLUGIN_INFO, context);
 
 		try {
 			sideMenu = ErodeDilateMenuController.create();
